1ae14fe1fcc097f1fc4fc87cf2851c1c0cefe913
ViViSpiro <scientia@vivispiro.ru>
2022-05-17 23:01:47 +0300

    1_1

---
+++ 1_1.py
@@ -0,0 +1,17 @@
+#создание переменных и вывод на экран:
+
+x = 13
+y = "red"
+z = "roses"
+print(x, y, z)
+
+#запрос у пользователя некоторых чиловых и строковых данных и вывод на экран:
+
+nickname = input("Enter your nickname: ")
+gender = input("Enter your gender: ")
+age = input("Enter your age: ")
+height = input("Enter your height: ")
+print("Your nickname is", nickname)
+print("Your gender is", gender)
+print("Your age is", age)
+print("Your height is", height)
