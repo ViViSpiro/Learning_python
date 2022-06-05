6bf54e7a793e896b761f47f31f5790f49b3f74ac
ViViSpiro <105007129+ViViSpiro@users.noreply.github.com>
2022-06-05 13:40:12 +0300

    Update 6_1.py

--- 6_1.py
+++ 6_1.py
@@ -5,9 +5,10 @@ class TrafficLight:
     __color = {"red": 7, "yellow": 2, "green": 6}
 
     def running(self):
-        for keys, values in self.__color.items():
-            print(keys)
-            sleep(values)
+        while True:
+            for keys, values in self.__color.items():
+                print(keys)
+                sleep(values)
 
 
 t = TrafficLight()
