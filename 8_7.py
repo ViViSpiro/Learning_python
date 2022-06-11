f51c1e2d19725d4916619bb87f399e15f37b63df
ViViSpiro <scientia@vivispiro.ru>
2022-06-11 17:08:46 +0300

    8_7

---
+++ 8_7.py
@@ -0,0 +1,28 @@
+import math
+
+
+class ComplexNumber:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        return "(%s+%sj)" % (self.a, self.b)
+
+    def __add__(self, other):
+        new_a = self.a + other.a
+        new_b = self.b + other.b
+        return ComplexNumber(new_a, new_b)
+
+    def __mul__(self, other):
+        new_a = self.a * other.a - self.b * other.b
+        new_b = self.b * other.a + self.a * other.b
+        return ComplexNumber(new_a, new_b)
+
+
+z1 = ComplexNumber(1, 2)
+z2 = ComplexNumber(2, 3)
+
+print(f"{z1} + {z2} = ", z1 + z2)
+print(f"{z1} * {z2} = ", z1 * z2)
